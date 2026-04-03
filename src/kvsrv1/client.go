e72ab35056da890ef090388ae2a6e3f410efdc40
Jackson <1974617416@qq.com>
2026-04-03 18:42:02 +0800

    Implement Get and Put methods in KVServer with proper error handling and synchronization

--- src/kvsrv1/client.go
+++ src/kvsrv1/client.go
@@ -2,10 +2,9 @@ package kvsrv
 
 import (
 	"6.5840/kvsrv1/rpc"
-	"6.5840/kvtest1"
-	"6.5840/tester1"
+	kvtest "6.5840/kvtest1"
+	tester "6.5840/tester1"
 )
-
 
 type Clerk struct {
 	clnt   *tester.Clnt
@@ -30,7 +29,37 @@ func MakeClerk(clnt *tester.Clnt, server string) kvtest.IKVClerk {
 // arguments. Additionally, reply must be passed as a pointer.
 func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
 	// You will have to modify this function.
-	return "", 0, rpc.ErrNoKey
+	for {
+		args := rpc.GetArgs{}
+		args.Key = key
+
+		reply := rpc.GetReply{}
+
+		ok := ck.clnt.Call(ck.server, "KVServer.Get", args, &reply)
+
+		if !ok {
+			continue
+		}
+
+		switch reply.Err {
+		case rpc.OK:
+			return reply.Value, reply.Version, reply.Err
+		case rpc.ErrMaybe:
+			continue
+		case rpc.ErrNoKey:
+			return "", reply.Version, reply.Err
+		case rpc.ErrVersion:
+			continue
+		case rpc.ErrWrongGroup:
+			continue
+		case rpc.ErrWrongLeader:
+			continue
+		default:
+			return "", 0, rpc.ErrNoKey
+		}
+
+	}
+
 }
 
 // Put updates key with value only if the version in the
@@ -52,5 +81,34 @@ func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
 // arguments. Additionally, reply must be passed as a pointer.
 func (ck *Clerk) Put(key, value string, version rpc.Tversion) rpc.Err {
 	// You will have to modify this function.
-	return rpc.ErrNoKey
+	var send_times uint8 = 0
+	for {
+		args := rpc.PutArgs{}
+		args.Key = key
+		args.Value = value
+		args.Version = version
+
+		reply := rpc.PutReply{}
+
+		ok := ck.clnt.Call(ck.server, "KVServer.Put", args, &reply)
+		send_times++
+		if !ok {
+			continue
+		}
+
+		switch reply.Err {
+		case rpc.OK:
+			return reply.Err
+		case rpc.ErrNoKey:
+			return reply.Err
+		case rpc.ErrVersion:
+			if send_times > 1 {
+				return rpc.ErrMaybe
+			} else {
+				return rpc.ErrVersion
+			}
+		default:
+			return rpc.ErrNoKey
+		}
+	}
 }
--- src/kvsrv1/server.go
+++ src/kvsrv1/server.go
@@ -6,7 +6,7 @@ import (
 
 	"6.5840/kvsrv1/rpc"
 	"6.5840/labrpc"
-	"6.5840/tester1"
+	tester "6.5840/tester1"
 )
 
 const Debug = false
@@ -18,16 +18,22 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
+type ValueType struct {
+	value_   string
+	version_ rpc.Tversion
+}
 
 type KVServer struct {
 	mu sync.Mutex
 
 	// Your definitions here.
+	kv_map_ map[string]ValueType
 }
 
 func MakeKVServer() *KVServer {
 	kv := &KVServer{}
 	// Your code here.
+	kv.kv_map_ = make(map[string]ValueType)
 	return kv
 }
 
@@ -35,6 +41,22 @@ func MakeKVServer() *KVServer {
 // exists. Otherwise, Get returns ErrNoKey.
 func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	key := args.Key
+	value, ok := kv.kv_map_[key]
+	if !ok {
+		// no key
+		reply.Err = rpc.ErrNoKey
+		return
+	}
+
+	// key exist
+	reply.Value = value.value_
+	reply.Version = value.version_
+	reply.Err = rpc.OK
+	return
 }
 
 // Update the value for a key if args.Version matches the version of
@@ -43,9 +65,44 @@ func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
 // args.Version is 0, and returns ErrNoKey otherwise.
 func (kv *KVServer) Put(args *rpc.PutArgs, reply *rpc.PutReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	value, ok := kv.kv_map_[args.Key]
+	if !ok {
+		// no key
+		if args.Version == 0 {
+			// new key
+			value_struct := ValueType{}
+			value_struct.value_ = args.Value
+			value_struct.version_ = args.Version + 1
+			kv.kv_map_[args.Key] = value_struct
+
+			reply.Err = rpc.OK
+			return
+		}
+
+		// error
+		reply.Err = rpc.ErrNoKey
+		return
+	}
+
+	// key exist
+	if value.version_ != args.Version {
+		// wrong version
+		reply.Err = rpc.ErrVersion
+		return
+	}
+
+	// version match
+	value_struct := ValueType{}
+	value_struct.value_ = args.Value
+	value_struct.version_ = args.Version + 1
+	kv.kv_map_[args.Key] = value_struct
+
+	reply.Err = rpc.OK
+	return
 }
-
-
 
 // You can ignore all arguments; they are for replicated KVservers
 func StartKVServer(tc *tester.TesterClnt, ends []*labrpc.ClientEnd, gid tester.Tgid, srv int, persister *tester.Persister) []any {
